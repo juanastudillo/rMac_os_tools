d7f8bc729d2ad25506dfa1a09278225593444625
Juan Astudillo <juanastudillo@hotmail.com>
2022-11-23 20:38:24 -0300

    Create python_upgrade_version.sh
    20221123 First commit

---
+++ python_upgrade_version.sh
@@ -0,0 +1,9 @@
+#Formato 
+python3.11 -m venv --upgrade <virtual environment>
+
+# Ejemplo
+python3.11 -m venv --upgrade vmci
+
+# En la creacion del environment , se pasa como parametro la version de python que queremos
+# Ejemplo 
+virtualenv -p python3.11 vmci
