88e558a4568fc95d4f4ec7ac5b90290b539312ee
Juan Astudillo <juanastudillo@hotmail.com>
2022-11-23 20:36:12 -0300

    Create macos_upgrade_clean.sh
    20221123 First commit

---
+++ macos_upgrade_clean.sh
@@ -0,0 +1,15 @@
+# Builded by  20220801
+# Juan Astudillo 
+# Improved for python development
+
+# Versioning
+# 20220922 comments to every command
+
+xcode-select --install      # upgrade xcode 
+pip install --upgrade pip   # upgrade pip base
+pip3 install --upgrade pip  # upgrade pip3 version
+softwareupdate -ai          # checks for updates to OS X itself and other associated applications, installing them upon approval.
+brew update                 # update homebrew itself and the packages list. 
+brew upgrade                # upgrade updates the actual packages to match the versions in the updated local git repository.
+brew upgrade --cask         # upgrade all casks/formulae which were previously listed as outdated.
+brew cleanup                # frees up space in macOS and Linux devices by removing old versions of formulae and small kegs of data (about 20-80 MB each). Remove old versions of installed software.
