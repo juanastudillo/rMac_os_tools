e04716fb6f44515ac5b5a509339d8882fb7dbb38
Juan Astudillo <juanastudillo@hotmail.com>
2024-04-29 18:36:55 -0400

    Create count_open_tabs.js
    First Commit 20240429

---
+++ count_open_tabs.js
@@ -0,0 +1,5 @@
+// hack to count the numbers of open tabs
+// In the address bar go to chrome://inspect/#pages
+
+// Run the following line of code in the javascript console:
+document.getElementById("pages-list").childElementCount
