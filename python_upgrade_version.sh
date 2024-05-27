07c9da08ed47131b5f8e67e4ef15613e202e3087
Juan Astudillo <juanastudillo@hotmail.com>
2024-05-27 04:48:28 -0400

    Update python_upgrade_version.sh
    improve example

--- python_upgrade_version.sh
+++ python_upgrade_version.sh
@@ -2,8 +2,8 @@
 python3.11 -m venv --upgrade <virtual environment>
 
 # Ejemplo
-python3.11 -m venv --upgrade vmci
+python3.11 -m venv --upgrade vweb
 
 # En la creacion del environment , se pasa como parametro la version de python que queremos
 # Ejemplo 
-virtualenv -p python3.11 vmci
+virtualenv -p python3.11 vweb
